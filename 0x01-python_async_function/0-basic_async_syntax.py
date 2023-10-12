4edb0fd612c1893eb212cd2baf1807edf1a5b657
garikaipaidamoyo <garikaipaidamoyo@gmail.com>
2023-10-12 12:50:01 +0000

    The basics of async

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env python3
-
+'''
+Module for asynchronous coroutine
+'''
 import asyncio
 import random
 
 
 async def wait_random(max_delay: int = 10) -> float:
+    '''
+    Waits for a random delay between 0 and max_delay seconds
+    and returns the delay as a float.
+    '''
     delay = random.uniform(0, max_delay)
     await asyncio.sleep(delay)
     return delay
-
-
-# Test the coroutine
-print(asyncio.run(wait_random()))
-print(asyncio.run(wait_random(5)))
-print(asyncio.run(wait_random(15)))
---
+++ 0x01-python_async_function/concurrent_coroutines.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+
+import asyncio
+from typing import List
+from .0-basic_async_syntax import wait_random  # Import the wait_random function correctly
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    coroutines = [wait_random(max_delay) for _ in range(n)]
+    results = await asyncio.gather(*coroutines)
+    return results
